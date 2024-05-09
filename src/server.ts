aea4e2547b3b2ffb0cfe59ef7e7c29648be71c24
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-05-09 19:58:00 +0600

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,17 @@
-import { Server } from 'http';
-import mongoose from 'mongoose';
-import app from './app';
-import seedSuperAdmin from './app/DB';
-import config from './app/config';
-
-let server: Server;
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-
-    seedSuperAdmin();
-    server = app.listen(config.port, () => {
-      console.log(`app is listening on port ${config.port}`);
+    await mongoose.connect(config.database_Url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
   }
 }
 
-main();
-
-process.on('unhandledRejection', (err) => {
-  console.log(`😈 unahandledRejection is detected , shutting down ...`, err);
-  if (server) {
-    server.close(() => {
-      process.exit(1);
-    });
-  }
-  process.exit(1);
-});
-
-process.on('uncaughtException', () => {
-  console.log(`😈 uncaughtException is detected , shutting down ...`);
-  process.exit(1);
-});
+// fozlerabbishuvo
+// gc4cFAR2q3vfvoxg
